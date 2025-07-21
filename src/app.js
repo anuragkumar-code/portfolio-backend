12db55d4e42e66e56445dd26b1fba19b33fce5d8
Anurag <anurag@itriangle.in>
2025-07-21 09:53:28 +0530

    added api key middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const apiKeyAuth = require('./middleware/apiKeyAuth');
 const uploadRoute = require('./routes/uploadRoute');
 const chatRoute = require('./routes/chatRoute');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(apiKeyAuth);
 
 app.use('/api/upload', uploadRoute);
 app.use('/api/chat', chatRoute);
