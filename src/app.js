9ca4fc167de1e3cc259153c7add201ba057baea3
Anurag <anurag@itriangle.in>
2025-07-24 19:24:56 +0530

    added wakeup api

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const apiKeyAuth = require('./middleware/apiKeyAuth');
 const uploadRoute = require('./routes/uploadRoute');
 const chatRoute = require('./routes/chatRoute');
+const wakeRoute = require('./routes/wakeRoute');
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(apiKeyAuth);
 
 app.use('/api/upload', uploadRoute);
 app.use('/api/chat', chatRoute);
+app.use('/api/wake', wakeRoute); 
 
 module.exports = app;
